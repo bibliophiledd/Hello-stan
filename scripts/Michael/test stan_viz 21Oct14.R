9ebc21906a4789bfc5c188de48b01eb9d5485527
mhandreae <mhandreae@gmail.com>
2014-10-21 08:35:40 -0400

    I tried to download the shiny stan package after updating to R 3.1 but got an error message

---
+++ scripts/Michael/test stan_viz 21Oct14.R
@@ -0,0 +1,6 @@
+
+setwd("C:/Users/Michael Andreae/Dropbox/Columbia/courses/Fall 2014/Stat Communication/project/Hello-stan/scripts/Michael")
+install.packages("devtools")
+library(devtools)
+install_github("jgabry/SHINYstan")
+library(SHINYstan)
