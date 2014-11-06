8af5e10f5ad77abbcf0fb4a0ea8f42f9b40f8278
mhandreae <mhandreae@gmail.com>
2014-11-06 09:18:33 -0500

    improved

--- scripts/Michael/test stan_viz 21Oct14.R
+++ scripts/Michael/test stan_viz 21Oct14.R
@@ -1,6 +1,13 @@
 
 setwd("C:/Users/Michael Andreae/Dropbox/Columbia/courses/Fall 2014/Stat Communication/project/Hello-stan/scripts/Michael")
-install.packages("devtools")
-library(devtools)
-install_github("jgabry/SHINYstan")
+
+## Need only one for the installation of the SHINYstan package 
+#install.packages("devtools")
+#library("devtools") 
+#install_github("jgabry/SHINYstan")
+
 library(SHINYstan)
+launch_SHINYstan_demo()
+launch_SHINYstan_demo(binormal)
+
+
